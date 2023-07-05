91cde8788ed5bf5029bd730f1eeb2313c1fa8bc4
Priyanka <priyanka@aaruraatechnologies.com>
2023-07-05 12:53:07 +0530

    pos client update

--- src/apiurl.tsx
+++ src/apiurl.tsx
@@ -2,7 +2,7 @@ import { API_URL } from "./config";
 
 export const COMMONAPI = (firstParam?: any, secondParam?: any) => {
     return {
-        GET: API_URL + `COMMONGET`,
+        GET: API_URL + `userlist`,
         PUT: API_URL + `COMMONPUT`,
         POST: API_URL + `COMMONPOST/${firstParam}/${secondParam}`,
         GETBYID: API_URL + `COMMONGETBYID`,
